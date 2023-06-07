bdbd272f29a98359dcd01a71f5e14aefc633b490
Cem Gezer <you@example.com>
2023-06-07 18:24:32 +0300

    Appcreate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ProfileCard from "./ProfileCard";
+
+function App(){
+    return(
+        <div>
+            <div>Personel Digital Assistans</div>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    );
+}
+
+export default App;
